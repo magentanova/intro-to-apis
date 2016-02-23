c0f4529b3a83db4cbc0776cd00bc18a98d41a882
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2016-02-22 19:24:03 -0600

    intial commit with full code

--- js/main.js
+++ js/main.js
@@ -1,45 +1,71 @@
 console.log($)
 
+var fetchDataPerQuery = function(zip) {
+  $.getJSON("http://congress.api.sunlightfoundation.com/legislators/locate?" + "zip=" + zip + "&apikey=7ba96d266cc84b168fab4d878d9aa141").then(function(d){
+    
+    var el = document.querySelector('#container')  
+    console.log(d)
+    var pols = d.results
 
+    var htmlStr = ''
 
-$.getJSON("http://congress.api.sunlightfoundation.com/legislators?apikey=7ba96d266cc84b168fab4d878d9aa141").then(function(d){
-  document.querySelector('#data-output-container pre').innerHTML = JSON.stringify(d,null,2)
+    for(var i = 0 ; i < pols.length ; i++){
+       var p = pols[i]
+       
+       htmlStr += '<div class="card pols">'
+       htmlStr += '<h3>'+p.first_name+' '+p.last_name+'</h3>'
+       htmlStr += '<h4>'+p.title +' -- '+ p.party+'-'+p.state_name+ '</h4>'
+       htmlStr += '<ul>'
+       htmlStr +=    "<li>"+ p.email +"</li>"
+       htmlStr +=    "<li>"+ p.website+"</li>"
+       htmlStr +=    "<li>"+ p.facebook_id+"</li>"
+       htmlStr +=    "<li> "+ p.twitter_id+"</li>"
+       htmlStr += '</ul>'
+       htmlStr += '<h5> Term End'+p.term_end+'</h5>'
+       htmlStr += '</div>'
+    } 
+
+    console.log(htmlStr)
+    document.querySelector('#container').innerHTML = htmlStr
+  })
+}
+
+document.querySelector('input').addEventListener('keypress',function(e) {
+  console.log(e.keyCode)
+  if (e.keyCode === 13) {
+
+      fetchDataPerQuery(e.target.value)
+  }
 })
 
+var fetchDataNoQuery = function() {
+  $.getJSON("http://congress.api.sunlightfoundation.com/legislators?apikey=7ba96d266cc84b168fab4d878d9aa141").then(function(d){
+    
+    var el = document.querySelector('#container')  
+    console.log(d)
+    var pols = d.results
 
+    var htmlStr = ''
 
+    for(var i = 0 ; i < pols.length ; i++){
+       var p = pols[i]
+       
+       htmlStr += '<div class="card pols">'
+       htmlStr += '<h3>'+p.first_name+' '+p.last_name+'</h3>'
+       htmlStr += '<h4>'+p.title +' -- '+ p.party+'-'+p.state_name+ '</h4>'
+       htmlStr += '<ul>'
+       htmlStr +=    "<li>"+ p.email +"</li>"
+       htmlStr +=    "<li>"+ p.website+"</li>"
+       htmlStr +=    "<li>"+ p.facebook_id+"</li>"
+       htmlStr +=    "<li> "+ p.twitter_id+"</li>"
+       htmlStr += '</ul>'
+       htmlStr += '<h5> Term End'+p.term_end+'</h5>'
+       htmlStr += '</div>'
+    } 
 
-$.getJSON("http://congress.api.sunlightfoundation.com/legislators?apikey=7ba96d266cc84b168fab4d878d9aa141").then(function(d){
-  
-  var el = document.querySelector('#container-part-3')  
-  console.log(el)
-  var pols = d.results
+    console.log(htmlStr)
+    document.querySelector('#container').innerHTML = htmlStr
+  })
+}
 
-
-  for(var i = 0 ; i < pols.length ; i++){
-     var p = pols[i], htmlStr=''
-     
-     htmlStr += '<div class="card pols">'
-     htmlStr += '<h3>'+p.first_name+' '+p.last_name+'</h3>'
-     htmlStr += '<h4>'+p.title +' -- '+ p.party+'-'+p.state_name+ '</h4>'
-     htmlStr += '<ul>'
-     htmlStr +=    "<li>"+ p.email +"</li>"
-     htmlStr +=    "<li>"+ p.website+"</li>"
-     htmlStr +=    "<li>"+ p.facebook_id+"</li>"
-     htmlStr +=    "<li> "+ p.twitter_id+"</li>"
-     htmlStr += '</ul>'
-     htmlStr += '<h5> Term End'+p.term_end+'</h5>'
-     htmlStr += '</div>'
-
-     el.innerHTML += htmlStr
-    //   <ul>
-    //     <li>email: </li>
-    //     <li>website: </li>
-    //     <li>facebook: </li>
-    //     <li>twitter: </li>
-    //   </ul>
-    //   <h6>term ends: DATE</h6>
-    // </div>
-  } 
-
-})
+fetchDataNoQuery()
